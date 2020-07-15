f92f761f3c3e056b1c1e198718cf51db3d292a6d
darot <darotudeen@gmail.com>
2020-07-15 15:23:34 +0100

    added registration screens

---
+++ app/src/main/java/com/peacedude/chattychat/ui/HomeFragment.kt
@@ -0,0 +1,53 @@
+package com.peacedude.chattychat.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.peacedude.chattychat.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    lateinit var registerBtn:Button
+    val signinBtn:Button by lazy {
+        signin_btn.findViewById<Button>(R.id.btn)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        registerBtn = register_btn.findViewById(R.id.btn)
+        registerBtn.setText("Register")
+        signinBtn.text = "Login"
+
+        registerBtn.setOnClickListener {
+            findNavController().navigate(R.id.registerFragment)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/peacedude/chattychat/ui/RegisterFragment.kt
@@ -0,0 +1,46 @@
+package com.peacedude.chattychat.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.peacedude.chattychat.R
+import kotlinx.android.synthetic.main.fragment_register.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterFragment : Fragment() {
+    val createAccountBtn by lazy {
+        create_an_account.findViewById(R.id.btn) as Button
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        createAccountBtn.text = "Create account"
+    }
+
+
+}
